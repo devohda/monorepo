a99c8e98f22c0a6fbd2efc26a1779c2afb7b0408
오다혜 <devohda123@gmail.com>
2025-08-20 17:08:07 +0900

    feat: cjs, esm 모두 대응

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,5 +1,1 @@
-export { default as Input } from "./input";
-export { default as Button } from "./button";
-
-export type * from "./input";
-export type * from "./button";
+export * from "./src";
---
+++ packages/react/src/index.ts
@@ -0,0 +1,5 @@
+export { default as Input } from "./input";
+export { default as Button } from "./button";
+
+export type * from "./input";
+export type * from "./button";
